8cb216ed89c85d58e51291d2d369d0d8e73f8abd
Timothy Sirmon <timsirmon@gmail.com>
2024-12-15 12:57:36 -0600

    Updated SECRET_KEY configuration for stability

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///topics.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default-key')
